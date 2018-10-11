e4e5fd44c63e28c36e79bf61f8e2458fa49abb7b
MoonCall <moonchang@postech.ac.kr>
2018-10-11 23:24:43 +0900

    add incorrect id/pw handling & loginAndCheckAttendance looping

--- config.ts
+++ config.ts
@@ -1,6 +1,33 @@
 const CONFIG = {
-    coinpanId: '',
-    coinpanPassword: '',
+    COINPAN_ID: '',
+    COINPAN_PASSWORD: '',
+    LOGIN_TRIAL_CYCLE_MSEC: 4 * 3600 * 1000,
 };
+
+function checkType(orig: any, target: string): string | number {
+    const origType = typeof orig;
+    switch (origType) {
+        case 'string':
+            return target.toString();
+        case 'number':
+            return Number(target);
+        default:
+            throw new Error(`${origType} type config is not overridable`);
+    }
+}
+
+for (const configKey in CONFIG) {
+    const envVarValue = process.env[configKey];
+    const configValue = CONFIG[configKey];
+    if (envVarValue != null) {
+        try {
+            CONFIG[configKey] = checkType(configValue, envVarValue);
+        } catch (err) {
+            console.error(err);
+            console.error(`configKey(${configKey}) is not overridable`);
+            process.exit(-1);
+        }
+    }
+}
 
 export default CONFIG;
--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 import * as puppeteer from 'puppeteer';
 import config from './config';
 
-async function loginCoinpanAndCheckAttendance() {
+async function loginCoinpanAndCheckAttendance(): Promise<void> {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
         await page.goto('https://www.coinpan.com');
     
-        await page.type('input.idpw_id', config.coinpanId);
-        await page.type('input.idpw_pass', config.coinpanPassword);
+        await page.type('input.idpw_id', config.COINPAN_ID);
+        await page.type('input.idpw_pass', config.COINPAN_PASSWORD);
     
+        page.on('dialog', async function (dialog) {
+            console.log(dialog.message());
+            await dialog.dismiss();
+            await browser.close();
+        });
+
         await page.evaluate(() => {
             (document.querySelector('.loginbutton > input[type="submit"]') as HTMLElement).click();
         });
@@ -22,6 +28,12 @@ async function loginCoinpanAndCheckAttendance() {
         await page.goto('https://coinpan.com/attendance');
 
         const pageContent = await page.content();
+        const loginCheckKeyword = '로그인을 하지 않았습니다.';
+        if (pageContent.indexOf(loginCheckKeyword) !== -1) {
+            await browser.close();
+            console.error("Failed to login");
+            return;
+        }
         const attendanceCheckKeyword = '출석이 완료되었습니다.';
         if (pageContent.indexOf(attendanceCheckKeyword) !== -1) {
             console.log(`attendanceCheckKeyword(${attendanceCheckKeyword}) was found on page. skip button click process`);
@@ -41,5 +53,20 @@ async function loginCoinpanAndCheckAttendance() {
     }
 }
 
-loginCoinpanAndCheckAttendance().then(() => console.log("DONE"));
+function reserveLoopingTrial(): void {
+    const tryLoginCoinpanAndCheckAttendanceThenReserveTrial = function () {
+        loginCoinpanAndCheckAttendance()
+        .then(function () {
+            console.log(`DONE: ${new Date()}`)
+            setTimeout(tryLoginCoinpanAndCheckAttendanceThenReserveTrial, config.LOGIN_TRIAL_CYCLE_MSEC);
+        })
+        .catch(function (err) {
+            console.error(err);
+            console.error(`FAILED: ${new Date()}`)
+            setTimeout(tryLoginCoinpanAndCheckAttendanceThenReserveTrial, config.LOGIN_TRIAL_CYCLE_MSEC);
+        });
+    };
+    tryLoginCoinpanAndCheckAttendanceThenReserveTrial();
+}
 
+reserveLoopingTrial();
